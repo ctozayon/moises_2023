81e27e8ef4e0f855800b13ff47f59c6b126d4ab6
Tairone <taironeamaral@bol.com.br>
2023-10-29 19:58:44 -0300

    ajuste layout

--- Admin/layouts/vendor-scripts.php
+++ Admin/layouts/vendor-scripts.php
@@ -6,3 +6,4 @@
 <script src="assets/libs/feather-icons/feather.min.js"></script>
 <!-- pace js -->
 <script src="assets/libs/pace-js/pace.min.js"></script>
+<script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
